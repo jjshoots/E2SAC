40955ac2fa13a932cad627374f1e1c33dbd7706d
Jet <taijunjet@hotmail.com>
2022-03-10 16:38:44 +0000

    action repeat for ant

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/fish
 if [ "$hostname" = "arctic-linx" ]
   ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
-  ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
+  # ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
 else if [ "$hostname" = "availab-dl1" ]
   source venv/bin/activate.fish
   xvfb-run -s '-screen 1 1400x900x24' python3 src/mainSAC.py --train --wandb --wandb_name='SAC' --notes='super small SAC ant'
