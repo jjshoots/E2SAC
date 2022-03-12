ed5609f4dbffe224cd16353a0df4ece149e62f3f
Jet <taijunjet@hotmail.com>
2022-03-12 15:21:14 +0000

    update

--- SAC/SACNet.py
+++ SAC/SACNet.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 from utils.neural_blocks import Neural_blocks
 
 
-
 class Actor(nn.Module):
     """
     Actor network
@@ -15,7 +14,7 @@ class Actor(nn.Module):
         super().__init__()
         self.num_actions = num_actions
 
-        _features_description = [28, 32, num_actions * 2]
+        _features_description = [28, 256, 256, num_actions * 2]
         _activation_description = ["lrelu"] * (len(_features_description) - 2) + [
             "identity"
         ]
@@ -38,33 +37,19 @@ class Critic(nn.Module):
         super().__init__()
         self.num_actions = num_actions
 
-        _features_description = [num_actions, 32]
-        _activation_description = ["identity"] * (len(_features_description) - 1)
-        self.action = Neural_blocks.generate_linear_stack(
-            _features_description, _activation_description
-        )
-
-        _features_description = [28, 32]
-        _activation_description = ["identity"] * (len(_features_description) - 1)
-        self.state = Neural_blocks.generate_linear_stack(
-            _features_description, _activation_description
-        )
-
-        _features_description = [32, 32, 1]
+        _features_description = [36, 256, 256, 1]
         _activation_description = ["lrelu"] * (len(_features_description) - 2) + [
             "identity"
         ]
-        self.merge = Neural_blocks.generate_linear_stack(
+        self.net = Neural_blocks.generate_linear_stack(
             _features_description, _activation_description
         )
 
     def forward(self, states, actions):
-        actions = self.action(actions)
-        states = self.state(states)
-
         if len(actions.shape) != len(states.shape):
-            states = states.unsqueeze(0)
+            states = torch.stack([states] * actions.shape[0], dim=0)
 
-        output = self.merge(states + actions)
+        output = torch.cat((states, actions), dim=-1)
+        output = self.net(output)
 
         return output
