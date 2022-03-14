2b631f6c54f437d8a75c966047d0e3ee9b55d079
Jet <taijunjet@hotmail.com>
2022-03-14 20:35:21 +0000

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/fish
 if [ "$hostname" = "arctic-linx" ]
-  # ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
+  ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
   ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
 else if [ "$hostname" = "availab-dl1" ]
   source venv/bin/activate.fish
