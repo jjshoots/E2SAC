b4a2715d95cd3d3efa775c153dd61e87007474c6
Jet <taijunjet@hotmail.com>
2022-03-21 13:18:12 +0000

    update

--- e2SAC/UASACNet.py
+++ e2SAC/UASACNet.py
@@ -46,7 +46,7 @@ class Actor(nn.Module):
 
     def forward(self, states):
         output = self.backbone(states)
-        output = self.net(output).reshape(*states.shape[:-1], 2, self.num_actions)
+        output = self.net(output).reshape(*output.shape[:-1], 2, self.num_actions)
         output = torch.movedim(output, -2, 0)
 
         return output
