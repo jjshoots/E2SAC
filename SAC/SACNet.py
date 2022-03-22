3ebe3a3d1deb542958c5c27b240ef07f487da68e
Jet <taijunjet@hotmail.com>
2022-03-22 14:17:25 +0000

    update

--- SAC/SACNet.py
+++ SAC/SACNet.py
@@ -15,7 +15,7 @@ class Actor(nn.Module):
         self.num_actions = num_actions
         self.state_size = state_size
 
-        _features_description = [state_size, 16, 16, num_actions * 2]
+        _features_description = [state_size, 256, 256, num_actions * 2]
         _activation_description = ["lrelu"] * (len(_features_description) - 2) + [
             "identity"
         ]
@@ -39,7 +39,7 @@ class Critic(nn.Module):
         self.num_actions = num_actions
         self.state_size = state_size
 
-        _features_description = [num_actions + state_size, 16, 16, 1]
+        _features_description = [num_actions + state_size, 256, 256, 1]
         _activation_description = ["lrelu"] * (len(_features_description) - 2) + [
             "identity"
         ]
--- run.sh
+++ run.sh
@@ -1,25 +1,43 @@
 #!/bin/fish
 if [ "$hostname" = "arctic-linx" ]
-  ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
-  ssh availab-dl1 'tmux send-keys -t 1 "./run.sh" ENTER'
-  ssh availab-dl1 'tmux send-keys -t 2 "./run.sh" ENTER'
-  ssh availab-dl1 'tmux send-keys -t 3 "./run.sh" ENTER'
-  ssh availab-dl1 'tmux send-keys -t 4 "./run.sh" ENTER'
-  ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
-  ssh availab-dl2 'tmux send-keys -t 1 "./run.sh" ENTER'
-  ssh availab-dl2 'tmux send-keys -t 2 "./run.sh" ENTER'
-  ssh availab-dl2 'tmux send-keys -t 3 "./run.sh" ENTER'
-  ssh availab-dl2 'tmux send-keys -t 4 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl1 'tmux send-keys -t 1 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl1 'tmux send-keys -t 2 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl1 'tmux send-keys -t 3 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl2 'tmux send-keys -t 1 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl2 'tmux send-keys -t 2 "./run.sh" ENTER'
+  # sleep 2
+  # ssh availab-dl2 'tmux send-keys -t 3 "./run.sh" ENTER'
+  # sleep 2
 else if [ "$hostname" = "availab-dl1" ]
   source venv/bin/activate.fish
-  xvfb-run -s '-screen 1 1400x900x24' python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
-  xvfb-run -s '-screen 2 1400x900x24' python3 src/mainSAC.py --train --env_name='HopperPyBulletEnv-v0' --wandb --wandb_name='SAC_hopper' --notes='hopper'
-  xvfb-run -s '-screen 3 1400x900x24' python3 src/mainSAC.py --train --env_name='HumanoidPyBulletEnv-v0' --wandb --wandb_name='SAC_humanoid' --notes='humanoid'
-  xvfb-run -s '-screen 4 1400x900x24' python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
+  python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
+  python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
+  python3 src/mainSAC.py --train --env_name='HopperPyBulletEnv-v0' --wandb --wandb_name='SAC_hopper' --notes='hopper'
+  python3 src/mainSAC.py --train --env_name='HopperPyBulletEnv-v0' --wandb --wandb_name='SAC_hopper' --notes='hopper'
+  python3 src/mainSAC.py --train --env_name='HumanoidPyBulletEnv-v0' --wandb --wandb_name='SAC_humanoid' --notes='humanoid'
+  python3 src/mainSAC.py --train --env_name='HumanoidPyBulletEnv-v0' --wandb --wandb_name='SAC_humanoid' --notes='humanoid'
+  python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
+  python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
 else if [ "$hostname" = "availab-dl2" ]
   source venv/bin/activate.fish
-  xvfb-run -s '-screen 1 1400x900x24' python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
-  xvfb-run -s '-screen 2 1400x900x24' python3 src/mainSAC.py --train --env_name='HopperPyBulletEnv-v0' --wandb --wandb_name='SAC_hopper' --notes='hopper'
-  xvfb-run -s '-screen 3 1400x900x24' python3 src/mainSAC.py --train --env_name='HumanoidPyBulletEnv-v0' --wandb --wandb_name='SAC_humanoid' --notes='humanoid'
-  xvfb-run -s '-screen 4 1400x900x24' python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
+  python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
+  python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
+  python3 src/mainSAC.py --train --env_name='HopperPyBulletEnv-v0' --wandb --wandb_name='SAC_hopper' --notes='hopper'
+  python3 src/mainSAC.py --train --env_name='HopperPyBulletEnv-v0' --wandb --wandb_name='SAC_hopper' --notes='hopper'
+  python3 src/mainSAC.py --train --env_name='HumanoidPyBulletEnv-v0' --wandb --wandb_name='SAC_humanoid' --notes='humanoid'
+  python3 src/mainSAC.py --train --env_name='HumanoidPyBulletEnv-v0' --wandb --wandb_name='SAC_humanoid' --notes='humanoid'
+  python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
+  python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
 end
+
+
+# xvfb-run -s '-screen 4 1400x900x24' python3 src/mainSAC.py --train --env_name='HalfCheetahPyBulletEnv-v0' --wandb --wandb_name='SAC_half_cheetah' --notes='half_cheetah'
