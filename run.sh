f62ed5d2f086604dc9c3b2b7dcc6b6f6affdcc14
Jet <taijunjet@hotmail.com>
2022-03-23 16:46:39 +0000

    update sync in and outs

--- run.sh
+++ run.sh
@@ -9,14 +9,14 @@ if [ "$hostname" = "arctic-linx" ]
   # sleep 2
   # ssh availab-dl1 'tmux send-keys -t 3 "./run.sh" ENTER'
   # sleep 2
-  # ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
-  # sleep 2
-  # ssh availab-dl2 'tmux send-keys -t 1 "./run.sh" ENTER'
-  # sleep 2
-  # ssh availab-dl2 'tmux send-keys -t 2 "./run.sh" ENTER'
-  # sleep 2
-  # ssh availab-dl2 'tmux send-keys -t 3 "./run.sh" ENTER'
-  # sleep 2
+  ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
+  sleep 2
+  ssh availab-dl2 'tmux send-keys -t 1 "./run.sh" ENTER'
+  sleep 2
+  ssh availab-dl2 'tmux send-keys -t 2 "./run.sh" ENTER'
+  sleep 2
+  ssh availab-dl2 'tmux send-keys -t 3 "./run.sh" ENTER'
+  sleep 2
 else if [ "$hostname" = "availab-dl1" ]
   source venv/bin/activate.fish
   python3 src/mainSAC.py --train --env_name='AntPyBulletEnv-v0' --wandb --wandb_name='SAC_ant' --notes='ant'
