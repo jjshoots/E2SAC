19d65a739237aaf19c7fb680c1794250caff88b0
Jet <taijunjet@hotmail.com>
2022-04-12 11:24:16 +0100

    fix sync

--- sync-in.sh
+++ sync-in.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-rsync -av --progress --include-from='rsync_include_in.txt' --exclude='*' availab-dl1:~/Sandboxes/e2SAC/ ./
-rsync -av --progress --include-from='rsync_include_in.txt' --exclude='*' availab-dl2:~/Sandboxes/e2SAC/ ./
+rsync -avr --progress availab-dl1:~/Sandboxes/e2SAC/weights/ ./weights
+rsync -avr --progress availab-dl1:~/Sandboxes/e2SAC/optim_weights/ ./optim_weights
+rsync -avr --progress availab-dl2:~/Sandboxes/e2SAC/weights/ ./weights
+rsync -avr --progress availab-dl2:~/Sandboxes/e2SAC/optim_weights/ ./optim_weights
