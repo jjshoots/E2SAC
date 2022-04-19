daab27e3beec5d92fb66964fb8303b469b97f536
Jet <taijunjet@hotmail.com>
2022-04-19 19:19:50 +0100

    update settings

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/fish
 if [ "$hostname" = "arctic-linx" ]
-  ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
+  # ssh availab-dl1 'tmux send-keys -t 0 "./run.sh" ENTER'
   ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
 else if [ "$hostname" = "availab-dl1" ]
   source venv/bin/activate.fish
-  python3 src/main.py --train --wandb --wandb_name='e2SAC_domain_randomize'
+  python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_reduced'
   # python3 src/mainSAC.py --train --wandb --wandb_name='SAC_domain_randomize'
 else if [ "$hostname" = "availab-dl2" ]
   source venv/bin/activate.fish
-  python3 src/main.py --train --wandb --wandb_name='e2SAC_domain_randomize'
+  python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_reduced'
   # python3 src/mainSAC.py --train --wandb --wandb_name='SAC_domain_randomize'
 end
