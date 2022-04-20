4b9669b20ebf71c02a8cd17c78b5f9dd435b3242
Jet <taijunjet@hotmail.com>
2022-04-21 00:17:09 +0100

    go back to normal settings and schedule runs

--- sync-out.sh
+++ sync-out.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rsync -avr --exclude-from='rsync_ignore_out.txt' ./ availab-dl1:~/Sandboxes/e2SAC/ --delete
 rsync -avr --exclude-from='rsync_ignore_out.txt' ./ availab-dl2:~/Sandboxes/e2SAC/ --delete
+rsync -avr --exclude-from='rsync_ignore_out.txt' ./ taijunjet1997@35.208.249.167:~/e2SAC/ --delete
