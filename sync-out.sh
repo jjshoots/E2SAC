635c718b7490ffa58897a4ed7d34d223921204fc
Jet <taijunjet@hotmail.com>
2022-04-22 19:54:25 +0100

    fix typo

--- sync-out.sh
+++ sync-out.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rsync -avr --progress --stats --exclude-from='rsync_ignore_out.txt' ./ availab-dl1:~/Sandboxes/e2SAC/ --delete
-rsync -avr --progress --stats --exclude-from='rsync_ignore_out.txt' ./ availab-dl2:~/Sandboxes/e2SAC/ --delete
+# rsync -avr --progress --stats --exclude-from='rsync_ignore_out.txt' ./ availab-dl1:~/Sandboxes/e2SAC/ --delete
+# rsync -avr --progress --stats --exclude-from='rsync_ignore_out.txt' ./ availab-dl2:~/Sandboxes/e2SAC/ --delete
+rsync -avr --progress --stats --exclude-from='rsync_ignore_out.txt' ./ taijunjet1997@35.208.249.167:~/e2SAC/ --delete -e 'ssh -i ~/.ssh/ssh-key-taijunjet.key'
