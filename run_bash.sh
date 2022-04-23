37c2b1eccda6ef1c9f8edcb49bd47cf2eab51cf8
Jet <taijunjet@hotmail.com>
2022-04-23 01:27:16 +0100

    update script for running on sulis

---
+++ run_bash.sh
@@ -0,0 +1,20 @@
+source venv/bin/activate
+
+declare -a pids=()
+
+python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_lessent' &
+# python3 src/mainSAC.py --train --wandb --wandb_name='SAC_dr_lessent' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_lessent' &
+# python3 src/mainSAC.py --train --wandb --wandb_name='SAC_dr_lessent' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_lessent' &
+# python3 src/mainSAC.py --train --wandb --wandb_name='SAC_dr_lessent' &
+pids+=($!)
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+# python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_lessent'
+# python3 src/mainSAC.py --train --wandb --wandb_name='SAC_dr_lessent'
