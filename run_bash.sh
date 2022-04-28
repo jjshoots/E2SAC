7acb012876d5e33a314fb735548c6fe7437ed7d9
Jet <taijunjet@hotmail.com>
2022-04-28 20:38:10 +0100

    change sulis config

--- run_bash.sh
+++ run_bash.sh
@@ -17,10 +17,19 @@ sleep 1
 python3 src/main.py --train --wandb --wandb_name='ant_cf1.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=1.0 &
 pids+=($!)
 sleep 1
-python3 src/main.py --train --wandb --wandb_name='ant_cf2.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=2.0 &
+python3 src/main.py --train --wandb --wandb_name='ant_cf0.01' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.01 &
 pids+=($!)
 sleep 1
-python3 src/main.py --train --wandb --wandb_name='ant_cf5.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=5.0 &
+python3 src/main.py --train --wandb --wandb_name='ant_cf0.05' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.05 &
+pids+=($!)
+sleep 1
+python3 src/main.py --train --wandb --wandb_name='ant_cf0.1' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.1 &
+pids+=($!)
+sleep 1
+python3 src/main.py --train --wandb --wandb_name='ant_cf0.5' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.5 &
+pids+=($!)
+sleep 1
+python3 src/main.py --train --wandb --wandb_name='ant_cf1.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=1.0 &
 pids+=($!)
 sleep 1
 
