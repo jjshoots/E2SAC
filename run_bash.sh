c7e7e6031a5a469ddcc9c5707334923623c1db77
Jet <taijunjet@hotmail.com>
2022-04-28 20:37:32 +0100

    change sulis runs

--- run_bash.sh
+++ run_bash.sh
@@ -2,22 +2,29 @@ source venv/bin/activate
 
 declare -a pids=()
 
+python3 src/main.py --train --wandb --wandb_name='ant_cf0.01' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.01 &
+pids+=($!)
+sleep 1
+python3 src/main.py --train --wandb --wandb_name='ant_cf0.05' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.05 &
+pids+=($!)
+sleep 1
 python3 src/main.py --train --wandb --wandb_name='ant_cf0.1' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.1 &
 pids+=($!)
+sleep 1
 python3 src/main.py --train --wandb --wandb_name='ant_cf0.5' --env_name=AntPyBulletEnv-v0 --confidence_lambda=0.5 &
 pids+=($!)
+sleep 1
 python3 src/main.py --train --wandb --wandb_name='ant_cf1.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=1.0 &
 pids+=($!)
+sleep 1
 python3 src/main.py --train --wandb --wandb_name='ant_cf2.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=2.0 &
 pids+=($!)
+sleep 1
 python3 src/main.py --train --wandb --wandb_name='ant_cf5.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=5.0 &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='ant_cf10.0' --env_name=AntPyBulletEnv-v0 --confidence_lambda=10.0 &
-pids+=($!)
+sleep 1
 
 for pid in ${pids[*]}; do
     wait $pid
 done
 
-# python3 src/main.py --train --wandb --wandb_name='e2SAC_dr_lessent'
-# python3 src/mainSAC.py --train --wandb --wandb_name='SAC_dr_lessent'
