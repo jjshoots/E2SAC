3ed5417bf7e1554441647a13860e77ae7a52778f
Jet <taijunjet@hotmail.com>
2022-04-29 16:45:57 +0100

    update

--- run.sh
+++ run.sh
@@ -5,10 +5,26 @@ if [ "$hostname" = "arctic-linx" ]
   ssh availab-dl2 'tmux send-keys -t 0 "./run.sh" ENTER'
 else if [ "$hostname" = "availab-dl1" ]
   source venv/bin/activate.fish
-  python3 src/mainESDDQN.py --train --wandb --wandb_name='lunarlander_test'
-  python3 src/mainESDDQN.py --train --wandb --wandb_name='lunarlander_test'
+  declare -a pids=()
+
+  python3 src/main.py --train --wandb --wandb_name='lunar_lander' &
+  pids+=($!)
+  python3 src/main.py --train --wandb --wandb_name='lunar_lander' &
+  pids+=($!)
+
+  for pid in ${pids[*]}; do
+      wait $pid
+  done
 else if [ "$hostname" = "availab-dl2" ]
   source venv/bin/activate.fish
-  python3 src/mainESDDQN.py --train --wandb --wandb_name='lunarlander_test'
-  python3 src/mainESDDQN.py --train --wandb --wandb_name='lunarlander_test'
+  declare -a pids=()
+
+  python3 src/main.py --train --wandb --wandb_name='lunar_lander' &
+  pids+=($!)
+  python3 src/main.py --train --wandb --wandb_name='lunar_lander' &
+  pids+=($!)
+
+  for pid in ${pids[*]}; do
+      wait $pid
+  done
 end
