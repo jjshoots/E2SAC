40e396f4631b5339777f3c36e0cddb21edb9aa17
Jet <taijunjet@hotmail.com>
2022-05-03 14:51:34 +0100

    update uri and plotting logic

--- src/plotter.py
+++ src/plotter.py
@@ -16,7 +16,9 @@ from matplotlib import rc
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set_style("white", {"axes.grid": False})
+sns.set_style("white")
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = ["Times New Roman"] + plt.rcParams["font.serif"]
 
 # See warnings only once
 import warnings
@@ -144,16 +146,19 @@ if __name__ == "__main__":
         "jjshoots/e2SAC_pybullet/12nev3lk",
     ]
     runs["E2SAC"] = [
-        "jjshoots/e2SAC_pybullet/26iy23z8",
-        "jjshoots/e2SAC_pybullet/z30or5e0",
-        "jjshoots/e2SAC_pybullet/2r6j4ti6",
-        "jjshoots/e2SAC_pybullet/1tahjde3",
-        "jjshoots/e2SAC_pybullet/8gef3h2b",
-        "jjshoots/e2SAC_pybullet/2ie3jxhp",
-        "jjshoots/e2SAC_pybullet/1936sb55",
-        "jjshoots/e2SAC_pybullet/3vq6jk5u",
-        "jjshoots/e2SAC_pybullet/28qlpnqd",
-        "jjshoots/e2SAC_pybullet/15h8pi1s",
+        # "jjshoots/pybullet_proper/3bg0jygk",
+        "jjshoots/pybullet_proper/1w6dc3wd",
+        "jjshoots/pybullet_proper/22hd0shm",
+        # "jjshoots/e2SAC_pybullet/26iy23z8",
+        # "jjshoots/e2SAC_pybullet/z30or5e0",
+        # "jjshoots/e2SAC_pybullet/2r6j4ti6",
+        # "jjshoots/e2SAC_pybullet/1tahjde3",
+        # "jjshoots/e2SAC_pybullet/8gef3h2b",
+        # "jjshoots/e2SAC_pybullet/2ie3jxhp",
+        # "jjshoots/e2SAC_pybullet/1936sb55",
+        # "jjshoots/e2SAC_pybullet/3vq6jk5u",
+        # "jjshoots/e2SAC_pybullet/28qlpnqd",
+        # "jjshoots/e2SAC_pybullet/15h8pi1s",
     ]
     run_list.append(runs)
     env_list.append("AntPyBulletEnv-v0")
@@ -174,16 +179,19 @@ if __name__ == "__main__":
         "jjshoots/e2SAC_pybullet/2ciy5u3x",
     ]
     runs["E2SAC"] = [
-        "jjshoots/e2SAC_pybullet/3v68x43e",
-        "jjshoots/e2SAC_pybullet/3dcmg6sb",
-        "jjshoots/e2SAC_pybullet/18c1nnmi",
-        "jjshoots/e2SAC_pybullet/3q62xhiu",
-        "jjshoots/e2SAC_pybullet/3tiax2q9",
-        "jjshoots/e2SAC_pybullet/1g6h6k8p",
-        "jjshoots/e2SAC_pybullet/2ihw442t",
-        "jjshoots/e2SAC_pybullet/brftgjqy",
-        "jjshoots/e2SAC_pybullet/2ttvam1p",
-        "jjshoots/e2SAC_pybullet/1cfh2ob3",
+        "jjshoots/pybullet_proper/cmos8rof",
+        "jjshoots/pybullet_proper/396k5lw4",
+        "jjshoots/pybullet_proper/2nvsj0t8",
+        # "jjshoots/e2SAC_pybullet/3v68x43e",
+        # "jjshoots/e2SAC_pybullet/3dcmg6sb",
+        # "jjshoots/e2SAC_pybullet/18c1nnmi",
+        # "jjshoots/e2SAC_pybullet/3q62xhiu",
+        # "jjshoots/e2SAC_pybullet/3tiax2q9",
+        # "jjshoots/e2SAC_pybullet/1g6h6k8p",
+        # "jjshoots/e2SAC_pybullet/2ihw442t",
+        # "jjshoots/e2SAC_pybullet/brftgjqy",
+        # "jjshoots/e2SAC_pybullet/2ttvam1p",
+        # "jjshoots/e2SAC_pybullet/1cfh2ob3",
     ]
     run_list.append(runs)
     env_list.append("HopperPyBulletEnv-v0")
@@ -204,16 +212,19 @@ if __name__ == "__main__":
         "jjshoots/e2SAC_pybullet/3siwabhk",
     ]
     runs["E2SAC"] = [
-        "jjshoots/e2SAC_pybullet/3s7i0gtm",
-        "jjshoots/e2SAC_pybullet/119e98d6",
-        "jjshoots/e2SAC_pybullet/3ncp4r5o",
-        "jjshoots/e2SAC_pybullet/2w5964ry",
-        "jjshoots/e2SAC_pybullet/2fns49hc",
-        "jjshoots/e2SAC_pybullet/1l1lbdzy",
-        "jjshoots/e2SAC_pybullet/27dnblfc",
-        "jjshoots/e2SAC_pybullet/a6i1t5al",
-        "jjshoots/e2SAC_pybullet/2e92ggz5",
-        "jjshoots/e2SAC_pybullet/35kzfwxw",
+        # "jjshoots/pybullet_proper/2deg8r3u",
+        "jjshoots/pybullet_proper/31udjhbm",
+        "jjshoots/pybullet_proper/1r1h9r90",
+        # "jjshoots/e2SAC_pybullet/3s7i0gtm",
+        # "jjshoots/e2SAC_pybullet/119e98d6",
+        # "jjshoots/e2SAC_pybullet/3ncp4r5o",
+        # "jjshoots/e2SAC_pybullet/2w5964ry",
+        # "jjshoots/e2SAC_pybullet/2fns49hc",
+        # "jjshoots/e2SAC_pybullet/1l1lbdzy",
+        # "jjshoots/e2SAC_pybullet/27dnblfc",
+        # "jjshoots/e2SAC_pybullet/a6i1t5al",
+        # "jjshoots/e2SAC_pybullet/2e92ggz5",
+        # "jjshoots/e2SAC_pybullet/35kzfwxw",
     ]
     run_list.append(runs)
     env_list.append("HalfCheetahPyBulletEnv-v0")
@@ -234,16 +245,19 @@ if __name__ == "__main__":
         "jjshoots/e2SAC_pybullet/1scupef7",
     ]
     runs["E2SAC"] = [
-        "jjshoots/e2SAC_pybullet/qy3fj41l",
-        "jjshoots/e2SAC_pybullet/2r2ocxx3",
-        "jjshoots/e2SAC_pybullet/3k7ipmip",
-        "jjshoots/e2SAC_pybullet/8vfdgp62",
-        "jjshoots/e2SAC_pybullet/3rq3f05i",
-        "jjshoots/e2SAC_pybullet/g5dtjuev",
-        "jjshoots/e2SAC_pybullet/32g555xp",
-        "jjshoots/e2SAC_pybullet/2fioechx",
-        "jjshoots/e2SAC_pybullet/1b9n57ov",
-        "jjshoots/e2SAC_pybullet/2ax8d0rd",
+        # "jjshoots/pybullet_proper/1kknkrdi",
+        "jjshoots/pybullet_proper/57udwl5w",
+        "jjshoots/pybullet_proper/z5n9vb5g",
+        # "jjshoots/e2SAC_pybullet/qy3fj41l",
+        # "jjshoots/e2SAC_pybullet/2r2ocxx3",
+        # "jjshoots/e2SAC_pybullet/3k7ipmip",
+        # "jjshoots/e2SAC_pybullet/8vfdgp62",
+        # "jjshoots/e2SAC_pybullet/3rq3f05i",
+        # "jjshoots/e2SAC_pybullet/g5dtjuev",
+        # "jjshoots/e2SAC_pybullet/32g555xp",
+        # "jjshoots/e2SAC_pybullet/2fioechx",
+        # "jjshoots/e2SAC_pybullet/1b9n57ov",
+        # "jjshoots/e2SAC_pybullet/2ax8d0rd",
     ]
     run_list.append(runs)
     env_list.append("Walker2DPyBulletEnv-v0")
