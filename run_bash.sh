5e7ea84e78bc0b29f57038cc2e09cabad2d188b9
Jet <taijunjet@hotmail.com>
2022-05-05 21:31:20 +0100

    update

--- run_bash.sh
+++ run_bash.sh
@@ -2,13 +2,25 @@ source venv/bin/activate
 
 declare -a pids=()
 
-python3 src/main.py --train --wandb --wandb_name='lunarlander' &
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='lunarlander' &
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='lunarlander' &
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='lunarlander' &
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
+pids+=($!)
+python3 src/main.py --train --wandb --wandb_name='cartpole' &
 pids+=($!)
 
 for pid in ${pids[*]}; do
