d4037706ad4185ce9401bf94025619ee3e061594
Jet <taijunjet@hotmail.com>
2022-05-06 12:12:05 +0100

    update run_bash

--- run_bash.sh
+++ run_bash.sh
@@ -4,39 +4,14 @@ source venv/bin/activate
 
 declare -a pids=()
 
-python3 src/main.py --train --wandb --wandb_name='ant' --env_name=AntPyBulletEnv-v0 &
+python3 src/main.py --train --wandb --wandb_name='ant_big' --env_name=AntPyBulletEnv-v0 &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='hopper' --env_name=HopperPyBulletEnv-v0 &
+python3 src/main.py --train --wandb --wandb_name='hopper_big' --env_name=HopperPyBulletEnv-v0 &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='walker' --env_name=Walker2DPyBulletEnv-v0 &
+python3 src/main.py --train --wandb --wandb_name='walker_big' --env_name=Walker2DPyBulletEnv-v0 &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='cheetah' --env_name=HalfCheetahPyBulletEnv-v0 &
+python3 src/main.py --train --wandb --wandb_name='cheetah_big' --env_name=HalfCheetahPyBulletEnv-v0 &
 pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='ant' --env_name=AntPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='hopper' --env_name=HopperPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='walker' --env_name=Walker2DPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='cheetah' --env_name=HalfCheetahPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='ant' --env_name=AntPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='hopper' --env_name=HopperPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='walker' --env_name=Walker2DPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='cheetah' --env_name=HalfCheetahPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='ant' --env_name=AntPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='hopper' --env_name=HopperPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='walker' --env_name=Walker2DPyBulletEnv-v0 &
-pids+=($!)
-python3 src/main.py --train --wandb --wandb_name='cheetah' --env_name=HalfCheetahPyBulletEnv-v0 &
-pids+=($!)
-
 for pid in ${pids[*]}; do
     wait $pid
 done
