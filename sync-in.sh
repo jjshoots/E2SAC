1ede98639926e1de690307aa7e6f4a1797827619
Jet <taijunjet@hotmail.com>
2022-05-15 03:30:06 +0100

    update

--- sync-in.sh
+++ sync-in.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rsync -av --progress --include-from='rsync_include_in.txt' --exclude='*' availab-dl1:~/Sandboxes/e2SAC/ ./
-rsync -av --progress --include-from='rsync_include_in.txt' --exclude='*' availab-dl2:~/Sandboxes/e2SAC/ ./
+# rsync -rav --progress --include-from='rsync_include_in.txt' --exclude='*' availab-dl1:~/Sandboxes/e2SAC/ ./
+# rsync -rav --progress --include-from='rsync_include_in.txt' --exclude='*' availab-dl2:~/Sandboxes/e2SAC/ ./
+rsync -rav --progress --exclude='*' --include-from='rsync_include_in.txt' sulis:~/e2SAC/ ./
