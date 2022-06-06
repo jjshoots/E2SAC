2bb1127861d585a837a0c363871a7ae43d37a913
Jet <taijunjet@hotmail.com>
2022-06-06 15:50:27 +0100

    update

--- run_availab.sh
+++ run_availab.sh
@@ -2,13 +2,28 @@
 source venv/bin/activate
 
 declare -a pids=()
-wandb agent jjshoots/DQN2/a0gjbznv --count 10 &
+wandb agent jjshoots/DQN2/u7k2k7qo --count 10 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/DQN2/a0gjbznv --count 10 &
+wandb agent jjshoots/DQN2/u7k2k7qo --count 10 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/DQN2/a0gjbznv --count 10 &
+wandb agent jjshoots/DQN2/u7k2k7qo --count 10 &
+pids+=($!)
+sleep 10
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+declare -a pids=()
+wandb agent jjshoots/DQN2/emhvyijs --count 10 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/DQN2/emhvyijs --count 10 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/DQN2/emhvyijs --count 10 &
 pids+=($!)
 sleep 10
 
