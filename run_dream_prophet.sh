c534164c1856c4f80a9382de1b8259737ca54e55
Jet <taijunjet@hotmail.com>
2022-06-20 12:26:02 +0100

    update

--- run_dream_prophet.sh
+++ run_dream_prophet.sh
@@ -2,46 +2,22 @@
 source venv/bin/activate
 
 declare -a pids=()
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/5bv1o5du --count 10 &
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/z7whmidz --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/5bv1o5du --count 10 &
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/z7whmidz --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/5bv1o5du --count 10 &
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/z7whmidz --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/5bv1o5du --count 10 &
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/z7whmidz --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/5bv1o5du --count 10 &
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/z7whmidz --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/5bv1o5du --count 10 &
-pids+=($!)
-sleep 10
-
-for pid in ${pids[*]}; do
-    wait $pid
-done
-
-declare -a pids=()
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/t3e9smkh --count 10 &
-pids+=($!)
-sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/t3e9smkh --count 10 &
-pids+=($!)
-sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/t3e9smkh --count 10 &
-pids+=($!)
-sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/t3e9smkh --count 10 &
-pids+=($!)
-sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/t3e9smkh --count 10 &
-pids+=($!)
-sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/t3e9smkh --count 10 &
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/z7whmidz --count 10 &
 pids+=($!)
 sleep 10
 
@@ -50,22 +26,46 @@ for pid in ${pids[*]}; do
 done
 
 declare -a pids=()
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/6ssn48ak --count 10 &
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/nc1ui7ok --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/6ssn48ak --count 10 &
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/nc1ui7ok --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/6ssn48ak --count 10 &
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/nc1ui7ok --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/6ssn48ak --count 10 &
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/nc1ui7ok --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/6ssn48ak --count 10 &
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/nc1ui7ok --count 10 &
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/6ssn48ak --count 10 &
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/nc1ui7ok --count 10 &
+pids+=($!)
+sleep 10
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+declare -a pids=()
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/7vjjj1qc --count 10 &
+pids+=($!)
+sleep 10
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/7vjjj1qc --count 10 &
+pids+=($!)
+sleep 10
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/DQN2/7vjjj1qc --count 10 &
+pids+=($!)
+sleep 10
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/7vjjj1qc --count 10 &
+pids+=($!)
+sleep 10
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/7vjjj1qc --count 10 &
+pids+=($!)
+sleep 10
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/DQN2/7vjjj1qc --count 10 &
 pids+=($!)
 sleep 10
 
