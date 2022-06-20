6e8012bab93daf8196abcfd534fb8c1cb2cebb98
Jet <taijunjet@hotmail.com>
2022-06-20 02:09:37 +0100

    update

--- run_availab.sh
+++ run_availab.sh
@@ -3,7 +3,7 @@ source venv/bin/activate
 
 declare -a pids=()
 
-python3 src/main.py --train --wandb --wandb_name='e2SAC' &
+python3 src/main.py --train --wandb --wandb_name='e2SAC_rand' &
 # python3 src/mainSAC.py --train --wandb --wandb_name='SAC' &
 pids+=($!)
 sleep 10
