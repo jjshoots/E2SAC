f5341676106400fdd3c2fde07a32df8cc60eb586
Jet <taijunjet@hotmail.com>
2022-06-30 14:16:33 +0100

    add plotting logic

--- src/plotter.py
+++ src/plotter.py
@@ -40,109 +40,104 @@ rcParams["ps.fonttype"] = 42
 rc("text", usetex=False)
 
 
-def get_wandb_log(run_uri, keys):
+def get_wandb_log(run, keys, start_val):
     assert isinstance(keys, list), "keys must be a list."
-    api = wandb.Api(timeout=30)
-    run = api.run(run_uri)
     history = run.scan_history(keys=keys)
 
     data = {}
     for key in keys:
         array = np.array([row[key] for row in history])
         array = array.astype(np.float64)
-        array = np.nan_to_num(array, nan=0.0, posinf=0.0, neginf=0.0)
+        array = np.nan_to_num(array, nan=start_val, posinf=start_val, neginf=start_val)
         data[key] = array
 
     return data
 
 
-if __name__ == "__main__":
+def process_sweep(sweep_uri, sweep_name, start_val=-100):
     # parameters
-    num_steps = 500000
-    num_intervals = 200
+    num_steps = 100000
+    num_intervals = 20
 
     # x_axis values to plot against
     x_axis = np.linspace(0, num_steps, num_intervals)
 
-    # list of algorithms and their corresponding uris
-    runs = {}
-    runs["Runtime Uncertainty"] = [
-        "jjshoots/carracing_discrete/16ghiz19",
-        "jjshoots/carracing_discrete/1t1i6i4z",
-        "jjshoots/carracing_discrete/ifbtm01s",
-        "jjshoots/carracing_discrete/2e01yqe5",
-        "jjshoots/carracing_discrete/q4mciaiy",
-        "jjshoots/carracing_discrete/evvbcntl",
-        "jjshoots/carracing_discrete/2ad054yx",
-        "jjshoots/carracing_discrete/85wv07fe",
-        "jjshoots/carracing_discrete/11d3jqat",
-        "jjshoots/carracing_discrete/1jla2e50",
-        "jjshoots/carracing_discrete/2otv5zh6",
-        "jjshoots/carracing_discrete/11d3jqat",
-    ]
+    # get the sweep handler
+    sweep = wandb.Api(timeout=30).sweep(sweep_uri)
 
-    # list of algorithms we have
-    algorithms = [key for key in runs]
+    # list for legend later
+    legend_list = []
+    legend_list.append(sweep_name)
 
     # load scores as dictionary mapping algorithms to their scores
     # each score is of size (num_runs x num_games x num_recordings)
-    scores = {}
-    for algorithm in runs:
-        score = []
-        for run_uri in runs[algorithm]:
-            log = get_wandb_log(run_uri, ["num_transitions", "runtime_uncertainty"])
-            score.append(
+    uncer_list = []
+    eval_list = []
+    for run in sweep.runs:
+        log = get_wandb_log(
+            run, ["num_transitions", "eval_perf", "runtime_uncertainty"], start_val
+        )
+        if len(log["eval_perf"]) > 0 and len(log["runtime_uncertainty"] > 0):
+            eval_list.append(
+                np.interp(x_axis, log["num_transitions"], log["eval_perf"])
+            )
+
+            uncer_list.append(
                 np.interp(x_axis, log["num_transitions"], log["runtime_uncertainty"])
             )
 
-        # stack along num_runs axis
-        score = np.stack(score, axis=0)
-        # expand along num_games axis
-        score = np.expand_dims(score, axis=1)
-
-        # add to overall scores
-        scores[algorithm] = score
+    # expand along num_games axis
+    eval_scores = {}
+    eval_scores[sweep_name] = np.expand_dims(eval_list, axis=1)
+    uncertainties = {}
+    uncertainties[sweep_name] = np.expand_dims(uncer_list, axis=1)
 
     # get interquartile mean
-    iqm = lambda scores: np.array(
-        [metrics.aggregate_iqm(scores[..., frame]) for frame in range(scores.shape[-1])]
+    iqm = lambda uncertainties: np.array(
+        [
+            metrics.aggregate_iqm(uncertainties[..., frame])
+            for frame in range(uncertainties.shape[-1])
+        ]
     )
     # compute confidence intervals
-    iqm_scores, iqm_cis = rly.get_interval_estimates(scores, iqm, reps=50000)
+    iqm_scores, iqm_cis = rly.get_interval_estimates(uncertainties, iqm, reps=50000)
 
     # plot sample efficiency curve
     plot_utils.plot_sample_efficiency_curve(
-        x_axis / 1e5,
+        x_axis / 1e3,
         iqm_scores,
         iqm_cis,
-        algorithms=algorithms,
-        xlabel=r"Timesteps (1e5)",
-        ylabel="Episodic Mean Epistemic Uncertainty",
+        algorithms=None,
+        xlabel=r"Timesteps (1e3)",
+        ylabel="Episodic Mean F-value",
         labelsize=24,
         ticklabelsize=24,
     )
 
-    plt.axvline(x=250000 / 1e5, color=sns.color_palette("colorblind")[1], linestyle="-")
-    algorithms.append("Domain Change")
+    # draw the vertical line at domain change
+    legend_list.append("Domain Change")
+    plt.axvline(x=50000 / 1e3, color=sns.color_palette("colorblind")[len(legend_list) - 1], linestyle="-")
 
     # form the legend
-    color_dict = dict(zip(algorithms, sns.color_palette("colorblind")))
+    color_dict = dict(zip(legend_list, sns.color_palette("colorblind")))
     fake_patches = [
-        patches.Patch(color=color_dict[alg], alpha=0.75) for alg in algorithms
+        patches.Patch(color=color_dict[alg], alpha=0.75) for alg in legend_list
     ]
     legend = plt.legend(
         fake_patches,
-        algorithms,
-        loc="lower right",
+        legend_list,
+        loc="lower center",
         fancybox=True,
-        ncol=len(algorithms),
-        fontsize="xx-large",
+        # ncol=len(legend_list),
+        fontsize=24,
         # bbox_to_anchor=(0.5, 1.1),
     )
 
-    # bound limites
-    plt.ylim(top=25)
-
-    # plt.title('Runtime Uncertainty CarRacing-v1', fontsize="xx-large")
+    plt.title('CarRacing w/ Domain Change F-value', fontsize=24)
+    plt.tight_layout()
     # plt.savefig('resource/RuntimeUncertaintyCarRacing.pdf')
     plt.show()
+
+
+if __name__ == "__main__":
+    process_sweep("jjshoots/carracing_discrete/s73mfuy2", "CarRacing w/ Domain Change")
