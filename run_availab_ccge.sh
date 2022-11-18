ad987647cae1db85a5f0c53545e3a75e6a538435
Jet <taijunjet@hotmail.com>
2022-11-18 15:32:03 +0000

    update sweep setup

--- run_availab_ccge.sh
+++ run_availab_ccge.sh
@@ -4,16 +4,16 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/ncnzlpx5 --count 12 &
+wandb agent jjshoots/CCGE2/28zczpg3 --count 12 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/ncnzlpx5 --count 12 &
+wandb agent jjshoots/CCGE2/28zczpg3 --count 12 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/d0tfkuci --count 12 &
+wandb agent jjshoots/CCGE2/igzeo4go --count 12 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/d0tfkuci --count 12 &
+wandb agent jjshoots/CCGE2/igzeo4go --count 12 &
 pids+=($!)
 sleep 10
 
