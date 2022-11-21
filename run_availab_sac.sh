d5d9ba50d0488d6ed04564b3a0a9acda4001e5cf
Jet <taijunjet@hotmail.com>
2022-11-21 19:03:51 +0000

    update sweep

--- run_availab_sac.sh
+++ run_availab_sac.sh
@@ -4,16 +4,16 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/n9zdxfjc --count 6 &
+wandb agent jjshoots/CCGE2/mrkz8wu2 --count 6 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/n9zdxfjc --count 6 &
+wandb agent jjshoots/CCGE2/mrkz8wu2 --count 6 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/n9zdxfjc --count 6 &
+wandb agent jjshoots/CCGE2/mrkz8wu2 --count 6 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/n9zdxfjc --count 6 &
+wandb agent jjshoots/CCGE2/mrkz8wu2 --count 6 &
 pids+=($!)
 sleep 10
 
