738701062daefda3951c5b7f9f4dfa3b306cd88a
jjshoots <taijunjet@hotmail.com>
2022-12-05 14:32:57 +0000

    udpate

--- src/mainSAC.py
+++ src/mainSAC.py
@@ -210,10 +210,10 @@ def setup_nets(wm: Wingman):
     has_weights, model_file, optim_file = wm.get_weight_files()
     if has_weights:
         # load the model
-        net.load_state_dict(torch.load(model_file, map_location=torch.device(cfg.config)))
+        net.load_state_dict(torch.load(model_file, map_location=torch.device(cfg.device)))
 
         # load the optimizer
-        checkpoint = torch.load(optim_file, map_location=torch.device(cfg.config))
+        checkpoint = torch.load(optim_file, map_location=torch.device(cfg.device))
 
         for opt_key in optim_set:
             optim_set[opt_key].load_state_dict(checkpoint["optim"][opt_key])
