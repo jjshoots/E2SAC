a16f7c8f6ae2274d8334d56cd4d9dad89eeaec5e
jjshoots <taijunjet@hotmail.com>
2022-12-21 12:33:54 +0000

    update

--- src/plotter_sweep.py
+++ src/plotter_sweep.py
@@ -168,10 +168,10 @@ if __name__ == "__main__":
     title = "Hopper-v4"
     sweep_uri_dict = {}
     sweep_uri_dict["SAC"] = "jjshoots/CCGE2/zgq81g05"
-    sweep_uri_dict["CCGE_1"] = "jjshoots/CCGE2/phevs4mc"
-    sweep_uri_dict["CCGE_2"] = "jjshoots/CCGE2/po10jfqp"
-    sweep_uri_dict["CCGE_2_NU"] = "jjshoots/CCGE2/18p6t19p"
-    sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/fscr3ka0"
+    sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/phevs4mc"
+    sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/po10jfqp"
+    sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/18p6t19p"
+    # sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/fscr3ka0"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 810.0
@@ -181,10 +181,10 @@ if __name__ == "__main__":
     title = "Ant-v4"
     sweep_uri_dict = {}
     sweep_uri_dict["SAC"] = "jjshoots/CCGE2/4uvx5qez"
-    sweep_uri_dict["CCGE_1"] = "jjshoots/CCGE2/dp4byhg8"
-    sweep_uri_dict["CCGE_2"] = "jjshoots/CCGE2/b0r4xjqu"
-    sweep_uri_dict["CCGE_2_NU"] = "jjshoots/CCGE2/f9sntmmm"
-    sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/5ydeyszy"
+    sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/dp4byhg8"
+    sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/b0r4xjqu"
+    sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/f9sntmmm"
+    # sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/5ydeyszy"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 810.0
@@ -194,10 +194,10 @@ if __name__ == "__main__":
     title = "HalfCheetah-v4"
     sweep_uri_dict = {}
     sweep_uri_dict["SAC"] = "jjshoots/CCGE2/95qoyb9w"
-    sweep_uri_dict["CCGE_1"] = "jjshoots/CCGE2/o61ibecs"
-    sweep_uri_dict["CCGE_2"] = "jjshoots/CCGE2/62jvni7q"
-    sweep_uri_dict["CCGE_2_NU"] = "jjshoots/CCGE2/5e3vqsse"
-    sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/3rk3ayzb"
+    sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/o61ibecs"
+    sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/62jvni7q"
+    sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/5e3vqsse"
+    # sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/3rk3ayzb"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 5240.0
@@ -207,10 +207,10 @@ if __name__ == "__main__":
     title = "Walker2d-v4"
     sweep_uri_dict = {}
     sweep_uri_dict["SAC"] = "jjshoots/CCGE2/aboikqup"
-    sweep_uri_dict["CCGE_1"] = "jjshoots/CCGE2/n6ufn853"
-    sweep_uri_dict["CCGE_2"] = "jjshoots/CCGE2/s34yh4a6"
-    sweep_uri_dict["CCGE_2_NU"] = "jjshoots/CCGE2/trgcinhi"
-    sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/4de8u2d3"
+    sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/n6ufn853"
+    sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/s34yh4a6"
+    sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/trgcinhi"
+    # sweep_uri_dict["CCGE_2_DO"] = "jjshoots/CCGE2/4de8u2d3"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 2250.0
