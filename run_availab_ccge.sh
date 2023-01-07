347dfc14f4c8a89d9d12086bede77be514b88801
jjshoots <taijunjet@hotmail.com>
2023-01-07 20:04:37 +0000

    im2 walker runs

--- run_availab_ccge.sh
+++ run_availab_ccge.sh
@@ -4,19 +4,19 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/8tex48bv --count 5 &
+wandb agent jjshoots/CCGE2/nzkwl5p3 --count 10 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/8tex48bv --count 5 &
+wandb agent jjshoots/CCGE2/nzkwl5p3 --count 10 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/8tex48bv --count 5 &
+wandb agent jjshoots/CCGE2/nzkwl5p3 --count 10 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/8tex48bv --count 5 &
+wandb agent jjshoots/CCGE2/nzkwl5p3 --count 10 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/8tex48bv --count 5 &
+wandb agent jjshoots/CCGE2/nzkwl5p3 --count 10 &
 pids+=($!)
 sleep 10
 
