603d579a8947281c4a407e41fa42b0f26a3f919a
snow-fox <taijunjet@hotmail.com>
2023-01-11 07:24:01 +0000

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/fish
 if [ "$hostname" = "snow-fox" ]
-  # ssh availab-dl1 'tmux send-keys -t 0 "./run_availab_ccge.sh" ENTER'
-  # ssh availab-dl2 'tmux send-keys -t 0 "./run_availab_ccge.sh" ENTER'
+  ./sync-out.sh
+  ssh availab-dl1 'tmux send-keys -t 0 "./run_availab_ccge.sh" ENTER'
+  ssh availab-dl2 'tmux send-keys -t 0 "./run_availab_ccge.sh" ENTER'
   ssh availab-dl3 'tmux send-keys -t 0 "./run_availab_ccge.sh" ENTER'
   ssh availab-dl4 'tmux send-keys -t 0 "./run_availab_ccge.sh" ENTER'
 else if [ "$hostname" = "availab-dl1" ]
--- run_availab_ccge.sh
+++ run_availab_ccge.sh
@@ -4,35 +4,16 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/fcm6kbat --count 3 &
+wandb agent jjshoots/CCGE2/7efxff2l --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/fcm6kbat --count 3 &
+wandb agent jjshoots/CCGE2/7efxff2l --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/fcm6kbat --count 3 &
+wandb agent jjshoots/CCGE2/7efxff2l --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/fcm6kbat --count 3 &
-pids+=($!)
-sleep 10
-
-for pid in ${pids[*]}; do
-    wait $pid
-done
-
-declare -a pids=()
-
-wandb agent jjshoots/CCGE2/w1poh5sj --count 3 &
-pids+=($!)
-sleep 10
-wandb agent jjshoots/CCGE2/w1poh5sj --count 3 &
-pids+=($!)
-sleep 10
-wandb agent jjshoots/CCGE2/w1poh5sj --count 3 &
-pids+=($!)
-sleep 10
-wandb agent jjshoots/CCGE2/w1poh5sj --count 3 &
+wandb agent jjshoots/CCGE2/7efxff2l --count 3 &
 pids+=($!)
 sleep 10
 
@@ -42,16 +23,16 @@ done
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/6htikfc6 --count 3 &
+wandb agent jjshoots/CCGE2/fk1ez9z0 --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/6htikfc6 --count 3 &
+wandb agent jjshoots/CCGE2/fk1ez9z0 --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/6htikfc6 --count 3 &
+wandb agent jjshoots/CCGE2/fk1ez9z0 --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/6htikfc6 --count 3 &
+wandb agent jjshoots/CCGE2/fk1ez9z0 --count 3 &
 pids+=($!)
 sleep 10
 
@@ -61,16 +42,35 @@ done
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/3qtvevee --count 3 &
+wandb agent jjshoots/CCGE2/62p2gthd --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/3qtvevee --count 3 &
+wandb agent jjshoots/CCGE2/62p2gthd --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/3qtvevee --count 3 &
+wandb agent jjshoots/CCGE2/62p2gthd --count 3 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/3qtvevee --count 3 &
+wandb agent jjshoots/CCGE2/62p2gthd --count 3 &
+pids+=($!)
+sleep 10
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+declare -a pids=()
+
+wandb agent jjshoots/CCGE2/m9ckqm0k --count 3 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/CCGE2/m9ckqm0k --count 3 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/CCGE2/m9ckqm0k --count 3 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/CCGE2/m9ckqm0k --count 3 &
 pids+=($!)
 sleep 10
 
