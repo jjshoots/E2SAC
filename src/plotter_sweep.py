c330c82cbce014f02bd721446077390f6c98c1f0
snow-fox <taijunjet@hotmail.com>
2023-01-12 09:35:44 +0000

    update plots

--- src/plotter_sweep.py
+++ src/plotter_sweep.py
@@ -170,7 +170,7 @@ if __name__ == "__main__":
     sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/phevs4mc"
     sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/18p6t19p"
     sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/po10jfqp"
-    # sweep_uri_dict["CCGE_B_Ex"] = "jjshoots/CCGE2/fscr3ka0"
+    sweep_uri_dict["CCGE_B_Ex"] = "jjshoots/CCGE2/fscr3ka0"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 850.0
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/dp4byhg8"
     sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/f9sntmmm"
     sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/b0r4xjqu"
-    # sweep_uri_dict["CCGE_B_Ex"] = "jjshoots/CCGE2/5ydeyszy"
+    sweep_uri_dict["CCGE_B_Ex"] = "jjshoots/CCGE2/5ydeyszy"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 980.0
@@ -198,7 +198,7 @@ if __name__ == "__main__":
     sweep_uri_dict["CCGE_1_Ex"] = "jjshoots/CCGE2/o61ibecs"
     sweep_uri_dict["CCGE_2_Im"] = "jjshoots/CCGE2/5e3vqsse"
     sweep_uri_dict["CCGE_2_Ex"] = "jjshoots/CCGE2/62jvni7q"
-    # sweep_uri_dict["CCGE_B_Ex"] = "jjshoots/CCGE2/3rk3ayzb"
+    sweep_uri_dict["CCGE_B_Ex"] = "jjshoots/CCGE2/3rk3ayzb"
 
     baselines_dict = {}
     baselines_dict["Oracle 1"] = 5400.0
