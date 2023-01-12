1cdfebc7a18974c6f709a11b920d4958e12059f8
snow-fox <taijunjet@hotmail.com>
2023-01-12 10:10:16 +0000

    add relocate code

--- run_availab.sh
+++ run_availab.sh
@@ -9,10 +9,13 @@ declare -a pids=()
 python3 src/generate_oracle.py --wandb --wandb_name="AdroitHandRelocateOracle" --train --env_name="AdroitHandRelocate-v1" &
 pids+=($!)
 sleep 10
-python3 src/generate_oracle.py --wandb --wandb_name="AdroitHandHammerOracle" --train --env_name="AdroitHandHammer-v1" &
+python3 src/generate_oracle.py --wandb --wandb_name="AdroitHandRelocateOracle" --train --env_name="AdroitHandRelocate-v1" &
 pids+=($!)
 sleep 10
-python3 src/generate_oracle.py --wandb --wandb_name="AdroitHandDoorOracle" --train --env_name="AdroitHandDoor-v1" &
+python3 src/generate_oracle.py --wandb --wandb_name="AdroitHandRelocateOracle" --train --env_name="AdroitHandRelocate-v1" &
+pids+=($!)
+sleep 10
+python3 src/generate_oracle.py --wandb --wandb_name="AdroitHandRelocateOracle" --train --env_name="AdroitHandRelocate-v1" &
 pids+=($!)
 sleep 10
 
