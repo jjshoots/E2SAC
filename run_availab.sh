62fc5a1ff95dc7cb33307aed15238144a8a53a8e
jjshoots <taijunjet@hotmail.com>
2023-01-13 17:56:10 +0000

    add sweep

--- run_availab.sh
+++ run_availab.sh
@@ -6,13 +6,13 @@ wingman-compress-weights
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2_oracle_search/995wvhyr --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2_oracle_search/995wvhyr --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2_oracle_search/995wvhyr --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
 pids+=($!)
 sleep 10
 
