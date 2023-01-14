c6318312c1994350ebf4f6cd93942e7161950f8c
jjshoots <taijunjet@hotmail.com>
2023-01-14 16:08:18 +0000

    udpdate

--- run_availab.sh
+++ run_availab.sh
@@ -15,6 +15,15 @@ sleep 10
 wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
 pids+=($!)
 sleep 10
+wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
+pids+=($!)
+sleep 10
+wandb agent jjshoots/CCGE2_oracle_search/grfpun8e --count 1 &
+pids+=($!)
+sleep 10
 
 for pid in ${pids[*]}; do
     wait $pid
