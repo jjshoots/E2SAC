1e73fc2a4119b43721ddf30c159a0dcedc1a3458
jjshoots <taijunjet@hotmail.com>
2023-01-19 15:50:05 +0000

    update sweep

--- run_availab.sh
+++ run_availab.sh
@@ -9,19 +9,19 @@ pip3 install -e . -r requirements.txt -U
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2_oracle_search/ztzbbcrw --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/svs7u7n4 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2_oracle_search/ztzbbcrw --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/svs7u7n4 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2_oracle_search/ztzbbcrw --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/svs7u7n4 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2_oracle_search/ztzbbcrw --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/svs7u7n4 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2_oracle_search/ztzbbcrw --count 1 &
+wandb agent jjshoots/CCGE2_oracle_search/svs7u7n4 --count 1 &
 pids+=($!)
 sleep 10
 
