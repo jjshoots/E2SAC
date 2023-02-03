b02e8426ff79473f2e5bb793d09f63f2ad94f7c8
snow-fox <taijunjet@hotmail.com>
2023-02-03 03:35:15 +0000

    update for fixedwing

--- sweep_setup/run_sweeps.sh
+++ sweep_setup/run_sweeps.sh
@@ -3,17 +3,18 @@
 # this file has been automatically generated, do not edit manually
 
 source venv/bin/activate
+pip3 uninstall pyflyt -y
 pip3 install -r requirements.txt -U
 wingman-compress-weights
 
 declare -a pids=()
-wandb agent jjshoots/CCGE2/qanjaxgs --count 6 & 
+wandb agent jjshoots/CCGE2_oracle_search/fi1jif20 --count 2 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/qanjaxgs --count 6 & 
+wandb agent jjshoots/CCGE2_oracle_search/fi1jif20 --count 2 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/qanjaxgs --count 6 & 
+wandb agent jjshoots/CCGE2_oracle_search/fi1jif20 --count 2 &
 pids+=($!)
 sleep 10
 
