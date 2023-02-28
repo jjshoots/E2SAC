8d098d8b15a51ea176bdca8e83a59a3cef1bc99a
jjshoots <taijunjet@hotmail.com>
2023-02-28 14:06:47 +0000

    update

--- sweep_setup/run_dream_prophet_sweep.sh
+++ sweep_setup/run_dream_prophet_sweep.sh
@@ -10,28 +10,28 @@ rm -rf weights/*
 wingman-compress-weights
 
 declare -a pids=()
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=0 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
-CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/rxdqkwex --count 2 & 
+CUDA_VISIBLE_DEVICES=1 wandb agent jjshoots/CCGE2/xmpdtqvh --count 2 & 
 pids+=($!)
 sleep 10
 
