be0737fc411b7d5b8198951d03e7961c5b509fa6
jjshoots <taijunjet@hotmail.com>
2023-03-21 19:30:44 +0000

    update rendered gifs

--- sync-in.sh
+++ sync-in.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rsync -avr --progress availab-dl1:~/Sandboxes/e2SAC/weights/Version288251/ ./weights/Version288251
+rsync -avr --progress prophet:~/e2SAC/weights/Version259518/ ./weights/Version259518
+# rsync -avr --progress availab-dl1:~/Sandboxes/e2SAC/weights/Version259158/ ./weights/Version259158
 # rsync -avr --progress availab-dl2:~/Sandboxes/e2SAC/weights/ ./weights
 # rsync -avr --progress availab-dl3:~/Sandboxes/e2SAC/weights/ ./weights
 # rsync -avr --progress availab-dl4:~/Sandboxes/e2SAC/weights/ ./weights
