f051f0a592f3507113074cfbdad3bf5edde2977c
snow-fox <taijunjet@hotmail.com>
2023-03-21 17:16:49 +0000

    update sync-in code

--- sync-in.sh
+++ sync-in.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# rsync -avr --progress availab-dl1:~/Sandboxes/e2SAC/weights/ ./weights
-rsync -avr --progress availab-dl2:~/Sandboxes/e2SAC/weights/Version607412/ ./weights/Version607412
+rsync -avr --progress availab-dl1:~/Sandboxes/e2SAC/weights/Version288251/ ./weights/Version288251
+# rsync -avr --progress availab-dl2:~/Sandboxes/e2SAC/weights/ ./weights
 # rsync -avr --progress availab-dl3:~/Sandboxes/e2SAC/weights/ ./weights
 # rsync -avr --progress availab-dl4:~/Sandboxes/e2SAC/weights/ ./weights
 
