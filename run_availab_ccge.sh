d48ec092eb0cfb7e22b0101fecdc2ae9a1516736
jjshoots <taijunjet@hotmail.com>
2023-05-20 14:57:56 +0100

    test ant long term

--- run_availab_ccge.sh
+++ run_availab_ccge.sh
@@ -4,16 +4,16 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/44l7ajlp --count 4 &
+wandb agent jjshoots/CCGE2/i5l7a9wm --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/44l7ajlp --count 4 &
+wandb agent jjshoots/CCGE2/i5l7a9wm --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/44l7ajlp --count 4 &
+wandb agent jjshoots/CCGE2/i5l7a9wm --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/44l7ajlp --count 4 &
+wandb agent jjshoots/CCGE2/i5l7a9wm --count 1 &
 pids+=($!)
 sleep 10
 
