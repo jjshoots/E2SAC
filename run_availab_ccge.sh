32f11a01b8b5a111eb107159ac6508a05e682761
jjshoots <taijunjet@hotmail.com>
2023-05-27 12:31:33 +0100

    update sweeps

--- run_availab_ccge.sh
+++ run_availab_ccge.sh
@@ -4,16 +4,16 @@ source venv/bin/activate
 
 declare -a pids=()
 
-wandb agent jjshoots/CCGE2/fp6vqi7q --count 1 &
+wandb agent jjshoots/CCGE2/sc5wfw83 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/fp6vqi7q --count 1 &
+wandb agent jjshoots/CCGE2/sc5wfw83 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/fp6vqi7q --count 1 &
+wandb agent jjshoots/CCGE2/sc5wfw83 --count 1 &
 pids+=($!)
 sleep 10
-wandb agent jjshoots/CCGE2/fp6vqi7q --count 1 &
+wandb agent jjshoots/CCGE2/sc5wfw83 --count 1 &
 pids+=($!)
 sleep 10
 
