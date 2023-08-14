4d42c804bc1b012a4bd43d3fd0ff14a0ab31a269
jjshoots <taijunjet@hotmail.com>
2023-08-14 01:00:34 +0100

    working config

--- sweep_setup/make_run_sweeps_sh.py
+++ sweep_setup/make_run_sweeps_sh.py
@@ -24,10 +24,8 @@ top_lines = """#!/bin/bash
 
 source venv/bin/activate
 
-pip3 install pyflyt -U
-
 pip3 uninstall pyflyt_dogfight -y
-pip3 install git+https://github.com/jjshoots/pyflyt_dogfight --no-cache-dir -U
+pip3 install git+https://github.com/jjshoots/pyflyt_dogfight@master --no-cache-dir -U
 
 wingman-compress-weights
 
--- sweep_setup/run_availab_sweep.sh
+++ sweep_setup/run_availab_sweep.sh
@@ -4,18 +4,16 @@
 
 source venv/bin/activate
 
-pip3 install pyflyt -U
-
 pip3 uninstall pyflyt_dogfight -y
-pip3 install git+https://github.com/jjshoots/pyflyt_dogfight --no-cache-dir -U
+pip3 install git+https://github.com/jjshoots/pyflyt_dogfight@master --no-cache-dir -U
 
 wingman-compress-weights
 
 declare -a pids=()
-wandb agent jjshoots/sac_dogfight/xxdnfn6e --count 1 & 
+wandb agent jjshoots/sac_dogfight/lzekb4tt --count 1 & 
 pids+=($!)
 sleep 10
-wandb agent jjshoots/sac_dogfight/xxdnfn6e --count 1 & 
+wandb agent jjshoots/sac_dogfight/lzekb4tt --count 1 & 
 pids+=($!)
 sleep 10
 
